0c34af3de290a1d74ce6ef4e65e78c6a9ee61b6d
Mintu Sikder <mintusikder15@gmail.com>
2024-11-24 23:33:17 +0600

    added image and navbar

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,11 +1,15 @@
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar";
+import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
---
+++ client/src/pages/Shared/Navbar.jsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
+import { BsHandbag } from "react-icons/bs";
+import logo from "../../assets/images/banner/logo.svg"
+const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/">Services</Link>
+      </li>
+      <li>
+        <Link to="/">Blog</Link>
+      </li>
+      <li>
+        <Link to="/">Contact</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navItems}
+          </ul>
+        </div>
+       <img className="h-12" src={logo} alt="" />
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <BsHandbag className="text-2xl mr-3" />
+        <IoIosSearch className="text-2xl mr-3" />
+        <button className="btn btn-outline btn-warning">Appointment</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
