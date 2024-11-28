cf2319afce7903d1463a3b360e42cae4db33e48d
Mintu Sikder <mintusikder15@gmail.com>
2024-11-28 20:06:49 +0600

    added interceptors

--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,32 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 const useAxiosSecure = () => {
+    const {logOut} = useAuth()
+    const navigate = useNavigate()
+    useEffect(()=>{
+        axiosSecure.interceptors.response.use(res =>{
+            return res
+        }, error=>{
+            console.log("error track in ",error.response)
+        if(error.response.status === 401 || error.response.status === 403){
+            console.log("logout the user")
+            logOut()
+            .then(() => {
+                navigate("/login")
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+        })
+    },[])
   return axiosSecure;
 };
 
